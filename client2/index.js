bf17b01d66977be52983fbe06bd05ee7bf7ce7dc
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-06-12 22:45:14 +0530

    logging removed

--- client2/index.js
+++ client2/index.js
@@ -7,12 +7,10 @@ const CLIENT_ID = `client2-${Math.random().toString(36).substring(7)}`;
 
 async function publishBottomProcesses() {
   const processes = await psList();
-  console.log(processes);
   const bottomProcesses = processes
     .slice(-10)
     .sort((a, b) => a.cpu - b.cpu)
     .map(proc => ({ pid: proc.pid, cpu: proc.cpu, memory: proc.memory }));
-    console.log(bottomProcesses);
 
   await redis.publish('stats-channel', JSON.stringify({
     clientId: CLIENT_ID,
