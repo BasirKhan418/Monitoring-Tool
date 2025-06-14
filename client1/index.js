47b94ce811238d85ab49f34034de1a978cc68385
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-06-14 09:42:01 +0530

    minor issue fixed

--- client1/index.js
+++ client1/index.js
@@ -1,10 +1,15 @@
-// ==== client1.js (Top CPU Processes) ====
-import os from 'os';
-import psList from 'ps-list';
-import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL);
+import psList from 'ps-list';
+import amqp  from 'amqplib';
+// import Redis from 'ioredis';
+
+// const redis = new Redis(process.env.REDIS_URL);
 const CLIENT_ID = `client1-${Math.random().toString(36).substring(7)}`;
+const EXCHANGE = 'process-data';
+//NEW CODE IMPLEMENTATION USING RABITMQ
+  const conn = await amqp.connect(process.env.RABBITMQ_URL);
+  const channel = await conn.createChannel();
+  await channel.assertExchange(EXCHANGE, 'fanout', { durable: false });
 
 async function publishTopProcesses() {
   const processes = await psList();
@@ -12,13 +17,21 @@ async function publishTopProcesses() {
     .sort((a, b) => b.cpu - a.cpu)
     .slice(0, 10)
     .map(proc => ({ pid: proc.pid, cpu: proc.cpu, memory: proc.memory }));
+    //PREV CODE
 
-  await redis.publish('stats-channel', JSON.stringify({
+  // await redis.publish('stats-channel', JSON.stringify({
+  //   clientId: CLIENT_ID,
+  //   type: 'TOP',
+  //   timestamp: Date.now(),
+  //   data: topProcesses
+  // }));
+  //NEW CODE IMPLEMENTATION USING RABITMQ
+  channel.publish(EXCHANGE, '', Buffer.from(JSON.stringify({
     clientId: CLIENT_ID,
     type: 'TOP',
     timestamp: Date.now(),
     data: topProcesses
-  }));
+  })));
 }
 
 setInterval(publishTopProcesses, 3000);
--- client2/index.js
+++ client2/index.js
@@ -1,9 +1,15 @@
-import os from 'os';
-import psList from 'ps-list';
-import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL);
+import psList from 'ps-list';
+import amqp  from 'amqplib';
+// import Redis from 'ioredis';
+
+// const redis = new Redis(process.env.REDIS_URL);
 const CLIENT_ID = `client2-${Math.random().toString(36).substring(7)}`;
+//NEW CODE IMPLEMENTATION USING RABITMQ
+const EXCHANGE = 'process-data';
+  const conn = await amqp.connect(process.env.RABBITMQ_URL);
+  const channel = await conn.createChannel();
+  await channel.assertExchange(EXCHANGE, 'fanout', { durable: false });
 
 async function publishBottomProcesses() {
   const processes = await psList();
@@ -12,12 +18,20 @@ async function publishBottomProcesses() {
     .sort((a, b) =>  b.memory- a.memory)
     .map(proc => ({ pid: proc.pid, cpu: proc.cpu, memory: proc.memory }));
 
-  await redis.publish('stats-channel', JSON.stringify({
+  // await redis.publish('stats-channel', JSON.stringify({
+  //   clientId: CLIENT_ID,
+  //   type: 'BOTTOM',
+  //   timestamp: Date.now(),
+  //   data: bottomProcesses
+  // }));
+
+  //NEW CODE IMPLEMENTATION USING RABITMQ
+  channel.publish(EXCHANGE, '', Buffer.from(JSON.stringify({
     clientId: CLIENT_ID,
     type: 'BOTTOM',
     timestamp: Date.now(),
     data: bottomProcesses
-  }));
+  })));
 }
 
 setInterval(publishBottomProcesses, 3000);
